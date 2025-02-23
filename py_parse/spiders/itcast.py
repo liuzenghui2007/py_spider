6b7630e7389b4f1eef8ff00457bf618c01ce270d
Your Name <you@example.com>
2025-02-23 12:20:28 +0800

    dev

--- py_parse/spiders/itcast.py
+++ py_parse/spiders/itcast.py
@@ -1,5 +1,5 @@
 import scrapy
-
+from py_parse.items import ItcastItem
 
 class ItcastSpider(scrapy.Spider):
     name = 'itcast'
@@ -9,3 +9,23 @@ class ItcastSpider(scrapy.Spider):
     def parse(self, response):
         filename = "teacher.html"
         open(filename, 'w').write(response.body.decode('utf-8'))
+        # 存放老师信息的集合
+        items = []
+
+        for each in response.xpath("//div[@class='li_txt']"):
+            # 将我们得到的数据封装到一个 `ItcastItem` 对象
+            item = ItcastItem()
+            #extract()方法返回的都是unicode字符串
+            name = each.xpath("h3/text()").extract()
+            title = each.xpath("h4/text()").extract()
+            info = each.xpath("p/text()").extract()
+
+            #xpath返回的是包含一个元素的列表
+            item['name'] = name[0]
+            item['title'] = title[0]
+            item['info'] = info[0]
+
+            items.append(item)
+
+        # 直接返回最后数据
+        return items
---
+++ readme.md
@@ -0,0 +1,14 @@
+
+保存数据
+scrapy保存信息的最简单的方法主要有四种，-o 输出指定格式的文件，命令如下：
+
+scrapy crawl itcast -o teachers.json
+json lines格式，默认为Unicode编码
+
+scrapy crawl itcast -o teachers.jsonl
+csv 逗号表达式，可用Excel打开
+
+scrapy crawl itcast -o teachers.csv
+xml格式
+
+scrapy crawl itcast -o teachers.xml
